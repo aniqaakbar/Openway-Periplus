f7e3c632f02ed5314aedde30e59594ca449130a6
aniqaakbar <110585105+aniqaakbar@users.noreply.github.com>
2024-12-31 22:10:49 +0700

    Update SeleniumTest.java

--- openway/src/test/java/SeleniumTest.java
+++ openway/src/test/java/SeleniumTest.java
@@ -44,28 +44,26 @@ public class SeleniumTest {
         Actions actions = new Actions(webDriver);
         actions.sendKeys(Keys.RETURN).perform();
 
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("preloader")));
         webDriver.findElement(By.xpath("//body/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/a[1]")).click();
-        /*Adding to cart*/
-        WebElement bookImage = webDriver.findElement(By.xpath("//img[@class='default-img' and @src='https://static.periplus.com/sErOmvNcQTv0lgIjm.Sl5TXZIi_dPtArYtqISrFIYg0X9JGJZDgzMQvZrcnDJvqqw--']"));
-//        String bookImageString = bookImage.getText();
-        Assert.assertTrue(bookImage.isDisplayed());
-        webDriver.findElement(By.xpath("//li[@class='active']/a[text()='Account Details']")).click();
-
-        WebElement cartQuantity = webDriver.findElement(By.id("cart-total-mobile"));
-        String cartQuantityString = cartQuantity.getText();
-        Assert.assertEquals(cartQuantityString, "1");
-
-        /*Checking the cart*/
-        webDriver.findElement(By.xpath("//*[@id='show-your-cart_mobile']/a")).click();
 
         WebElement bookPrice = webDriver.findElement(By.xpath("//span[@class='special' and contains(text(), 'Rp 505,000')]"));
-        WebElement cartPrice = webDriver.findElement(By.id("sub_total"));
+
+        /*Adding to cart*/
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("preloader")));
+        webDriver.findElement(By.xpath("//button[contains(text(),'Add to Cart')]")).click();
+
+//        WebElement cartQuantity = webDriver.findElement(By.xpath("//span[@id='cart_total_mobile']"));
+//        String cartQuantityString = cartQuantity.getText();
+//        Assert.assertEquals(cartQuantityString, "1");
+
+        /*Checking the cart*/
+        webDriver.findElement(By.xpath("//header/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/a[1]")).click();
+        WebElement cartPrice = webDriver.findElement(By.xpath("//span[@id='sub_total']"));
 
         String bookPriceString = bookPrice.getText();
         String cartPriceString = cartPrice.getText();
         Assert.assertEquals(bookPriceString, cartPriceString);
-
-        Assert.assertTrue(bookImage.isDisplayed());
 
         webDriver.close();
     }
